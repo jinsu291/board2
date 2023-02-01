bcd6d50e3c5f02767d3308fd82e467918345c0fd
jinsu291 <wlstn291@naver.com>
2023-02-01 15:02:50 +0900

    make article - detail, list

---
+++ src/main/java/com/example/board2/MainController.java
@@ -0,0 +1,28 @@
+package com.example.board2;
+
+import com.example.board2.article.entity.Article;
+import com.example.board2.article.service.ArticleService;
+import com.example.board2.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/")
+@RequiredArgsConstructor
+@Controller
+public class MainController {
+
+    private final ArticleService articleService;
+    private final UserService userService;
+
+    @GetMapping("/")
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Article> paging = this.articleService.getList(page);
+        model.addAttribute("paging", paging);
+        return "article/list";
+    }
+}
