5aadc2b8c517cd025b58a1a32d3bfbb32d1c4c80
jinsu291 <wlstn291@naver.com>
2023-02-01 12:04:48 +0900

    modify.. and i will make user

--- src/main/java/com/example/board2/article/controller/ArticleController.java
+++ src/main/java/com/example/board2/article/controller/ArticleController.java
@@ -1,14 +1,19 @@
 package com.example.board2.article.controller;
 
 import com.example.board2.article.entity.Article;
+import com.example.board2.article.form.ArticleForm;
 import com.example.board2.article.service.ArticleService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
 
 @RequestMapping("/article")
 @RequiredArgsConstructor
@@ -21,6 +26,64 @@ public class ArticleController {
     public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
         Page<Article> paging = this.articleService.getList(page);
         model.addAttribute("paging", paging);
-        return "question/list";
+        return "article/list";
     }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Article article = this.articleService.getArticle(id);
+        model.addAttribute("article", article);
+        return "article/detail";
+    }
+
+    @GetMapping("/create")
+    public String articleCreate(ArticleForm articleForm) {
+        return "article/form.html";
+    }
+
+    @PostMapping("/create")
+    public String articleCreate(@Valid ArticleForm articleForm, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "article/form.html";
+        }
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.articleService.create(articleForm.getSubject(), articleForm.getContent(), siteUser);
+        return "redirect:/article/list";
+    }
+
+    @GetMapping("/modify/{id}")
+    public String articleModify(ArticleForm articleForm, @PathVariable("id") Integer id, Principal principal) {
+        Article article = this.articleService.getArticle(id);
+        if(!article.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        articleForm.setSubject(article.getSubject());
+        articleForm.setContent(article.getContent());
+        return "article/form";
+    }
+
+    @PostMapping("/modify/{id}")
+    public String articleModify(@Valid ArticleForm articleForm, BindingResult bindingResult,
+                                 Principal principal, @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "article/form";
+        }
+        Article article = this.articleService.getArticle(id);
+        if (!article.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.articleService.modify(article, articleForm.getSubject(), articleForm.getContent());
+        return String.format("redirect:/article/detail/%s", id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public String articleDelete(Principal principal, @PathVariable("id") Integer id) {
+        Article article = this.articleService.getArticle(id);
+        if (!article.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.articleService.delete(article);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/example/board2/article/service/ArticleService.java
+++ src/main/java/com/example/board2/article/service/ArticleService.java
@@ -1,6 +1,7 @@
 package com.example.board2.article.service;
 
 import com.example.board2.article.entity.Article;
+import com.example.board2.article.exception.DataNotFoundException;
 import com.example.board2.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -23,5 +25,14 @@ public class ArticleService {
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.articleRepository.findAll(pageable);
+    }
+
+    public Article getArticle(Integer id) {
+        Optional<Article> question = this.articleRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
